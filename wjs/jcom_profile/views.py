b3d7ef134e628097a14be3f236a2ce67fc540bc9
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-04-27 14:15:39 +0200

    Must use JCOMProfile not just a "SimpleLazyObject".

--- wjs/jcom_profile/views.py
+++ wjs/jcom_profile/views.py
@@ -1,13 +1,15 @@
 """My views. Looking for a way to "enrich" Janeway's `edit_profile`."""
 from django.shortcuts import render
 from wjs.jcom_profile.forms import JCOMProfileForm
+from wjs.jcom_profile.models import JCOMProfile
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def prova(request):
     """Una prova."""
-    user = request.user
+    user = JCOMProfile.objects.get(pk=request.user.id)
     form = JCOMProfileForm(instance=user)
+    # import ipdb; ipdb.set_trace()
     context = dict(form=form, user_to_edit=user)
     return render(request, 'aaa.html', context)
