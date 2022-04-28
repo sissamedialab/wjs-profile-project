d69a2fcd1de636c39d87aaaf16eed4022a0130b6
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-04-28 14:30:44 +0200

    Probably found how to create&link Account+JCOMProfile

--- wjs/jcom_profile/models.py
+++ wjs/jcom_profile/models.py
@@ -20,7 +20,16 @@ class JCOMProfile(Account):
     """An enrichment of Janeway's Account."""
 
     objects = AccountManager()
-
+    # The following is redundant.
+    # If not explicitly given, django creates a OTOField
+    # named account_id_ptr.
+    # But then I'm not sure how I should link the two:
+    # see signals.py
+    janeway_account = models.OneToOneField(
+        Account,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     profession = models.IntegerField(
         null=False,
         # If there is no "default", when an Account is created, a
--- wjs/jcom_profile/signals.py
+++ wjs/jcom_profile/signals.py
@@ -1,6 +1,6 @@
 """Signals.
 
-Every time a user model instance is created, a corresponding user
+Every time a user model instance is created, a corresponding JCOM
 profile instance must be created as well.
 
 """
@@ -13,16 +13,17 @@ from wjs.jcom_profile.models import JCOMProfile
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_profile_handler(sender, instance, created, **kwargs):
-    """Handle AccountProfession creation (if necessary)."""
+    """Create the JCOM profile, only if the Account is newly created."""
     if not created:
         return
-    # Create the profile object, only if it is newly created
 
     # TODO: move defalt to model OR
     # change the user-creation form OR
     # do something else?
     default_profession = 3
-    profile = JCOMProfile(profession=default_profession)
-    profile.save()
-    instance.profession = profile
+    JCOMProfile(
+        janeway_account=instance,
+        profession=default_profession).save()
+    # If I don't `save()` the instance again, an empty record is created
+    # (not sure what's happening here...)
     instance.save()
