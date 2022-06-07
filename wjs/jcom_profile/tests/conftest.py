d1f56bb1a4ab35816c720160ea1427f94fd26946
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-06-07 12:23:10 +0200

    Safekeep these, but they don't seem to be the right path.

--- wjs/jcom_profile/tests/conftest.py
+++ wjs/jcom_profile/tests/conftest.py
@@ -11,16 +11,16 @@
 import pytest
 
 
-@pytest.fixture(scope='session')
-def django_db_setup():
-    """Avoid creating/setting up the test database."""
-    pass
+# @pytest.fixture(scope='session')
+# def django_db_setup():
+#     """Avoid creating/setting up the test database."""
+#     pass
 
 
-@pytest.fixture
-def db_access_without_rollback_and_truncate(
-        request,
-        django_db_setup, django_db_blocker):
-    """Do not clean the DB."""
-    django_db_blocker.unblock()
-    request.addfinalizer(django_db_blocker.restore)
+# @pytest.fixture
+# def db_access_without_rollback_and_truncate(
+#         request,
+#         django_db_setup, django_db_blocker):
+#     """Do not clean the DB."""
+#     django_db_blocker.unblock()
+#     request.addfinalizer(django_db_blocker.restore)
