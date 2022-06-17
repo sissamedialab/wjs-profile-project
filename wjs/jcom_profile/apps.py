bdeb4192b74db3ca0aff6aa40119e0a404fc342f
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-06-17 16:23:37 +0200

    Register hooks.
    TODO: refactor registration code on Janeway's side.

--- wjs/jcom_profile/apps.py
+++ wjs/jcom_profile/apps.py
@@ -1,4 +1,8 @@
+"""Configure this application."""
+# https://docs.djangoproject.com/en/4.0/ref/applications/
 from django.apps import AppConfig
+from django.conf import settings
+import logging
 
 
 class JCOMProfileConfig(AppConfig):
@@ -9,6 +13,33 @@ class JCOMProfileConfig(AppConfig):
 
     def ready(self):
         """Call during initialization."""
+        # import ipdb; ipdb.set_trace()
         from wjs.jcom_profile import signals
         # from wjs.jcom_profile import monkey_patching
         from wjs.jcom_profile import urls
+
+        logging.warning("✨CALLED")
+        self.register_hooks()
+
+    def register_hooks(self):
+        """Register my functions to Janeway's hooks."""
+        hooks = [
+            dict(nav_block=dict(module='wjs.jcom_profile.hooks',
+                                function='prova_hook')),
+        ]
+        # import ipdb; ipdb.set_trace()
+        # from core/plugin_loader.py:64
+        # Register plugin hooks
+        if settings.PLUGIN_HOOKS:
+            super_hooks = settings.PLUGIN_HOOKS
+        else:
+            settings.PLUGIN_HOOKS = {}
+            super_hooks = {}
+
+        for _dict in hooks:
+            if _dict:
+                for k, v in _dict.items():
+                    super_hooks.setdefault(k, []).append(v)
+
+        for k, v in super_hooks.items():
+            settings.PLUGIN_HOOKS[k] = v
