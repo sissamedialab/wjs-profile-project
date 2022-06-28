68006c1e1c8cc0de2f893fe9b8cea6b5f6c21901
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-06-28 12:45:07 +0200

    Add a table to store usercods for imported users.
    Accounts can be imported from several sources.

--- wjs/jcom_profile/models.py
+++ wjs/jcom_profile/models.py
@@ -3,16 +3,23 @@
 from django.db import models
 from core.models import Account
 from core.models import AccountManager
+
 # TODO: use settings.AUTH_USER_MODEL
 # from django.conf import settings
 
 PROFESSIONS = (
-    (0, 'A researcher in S&T studies,'
-     ' science communication or neighbouring field'),
-    (1, 'A practitioner in S&T'
-     ' (e.g. journalist, museum staff, writer, ...)'),
-    (2, 'An active scientist'),
-    (3, 'Other'),
+    (
+        0,
+        "A researcher in S&T studies,"
+        " science communication or neighbouring field",
+    ),
+    (
+        1,
+        "A practitioner in S&T"
+        " (e.g. journalist, museum staff, writer, ...)",
+    ),
+    (2, "An active scientist"),
+    (3, "Other"),
 )
 
 
@@ -33,6 +40,49 @@ class JCOMProfile(Account):
     # Even if EO wants "profession" to be mandatory, we cannot set it
     # to `null=False` (i.e. `NOT NULL` at DB level) because we do not
     # have this data for most of our existing users.
-    profession = models.IntegerField(
-        null=True,
-        choices=PROFESSIONS)
+    profession = models.IntegerField(null=True, choices=PROFESSIONS)
+
+
+class UserCod(models.Model):
+    """Storage area for wjapp, PoS, SGP,... userCods."""
+
+    # TODO: drop pk and use the three fields as pk
+
+    account = models.ForeignKey(
+        to=Account, on_delete=models.CASCADE, related_name="usercods"
+    )
+    userCod = models.CharField(max_length=100)
+
+    # django >= 3.0
+    # class Sources(models.IntegerChoices):
+    #     """Source of the userCod."""
+    #     jhep = 0
+    #     pos = 1
+    #     jcap = 2
+    #     jinst = 3
+    #     jstat = 4
+    #     jcom = 5
+    #     jcomal = 6
+    #     sgp = 7
+    # source = models.IntegerField(choices=Sources.choices)
+
+    sources = (
+        (0, "jhep"),
+        (1, "pos"),
+        (2, "jcap"),
+        (3, "jinst"),
+        (4, "jstat"),
+        (5, "jcom"),
+        (6, "jcomal"),
+        (7, "sgp"),
+    )
+    source = models.IntegerField(choices=sources)
+
+    class Meta:
+        """Model's Meta."""
+
+        # django >= 2...
+        # constraints = [
+        #     models.UniqueConstraint(fields=("account", "userCod", "source")),
+        # ]
+        unique_together = ("account", "userCod", "source")
