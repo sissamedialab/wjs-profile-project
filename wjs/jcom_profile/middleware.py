84dbca24999eb6b8fd9999ee053ceab0e59c9e4a
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-09-27 14:47:44 +0000

    Include middleware to enforce acknowledgment of privacy policy

---
+++ wjs/jcom_profile/middleware.py
@@ -0,0 +1,64 @@
+"""Middleware for JCOM account profile."""
+from django.contrib import messages
+from django.shortcuts import redirect, reverse
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class PrivacyAcknowledgedMiddleware:
+    """Ensure that the logged-in user has acknowledged the privacy policy."""
+
+    @staticmethod
+    def process_request(request):
+        """Ensure that the logged-in user has acknowledged the privacy policy.
+
+        Kick in only if there is a logged-in user (otherwise return None).
+        Let alone /logout and /profile.
+
+        If the logged-in user hasn't got a gdpr_policy flag, set a
+        flash message and redirect to /profile.
+
+        """
+        if not hasattr(request, "user"):
+            return None
+
+        # The following fails on <J-CODE>/profile
+        # if request.path in ("/logout/", "/profile/"): <--
+
+        # The following fails on <J-CODE>/*
+        # (does the resolver know about journals?)
+        # match = resolve(request.path)
+        # if match.url_name in (
+        #     "core_edit_profile",
+        #     "core_logout",
+        # ):
+        if request.path.endswith("/logout/") or request.path.endswith(
+            "/profile/"
+        ):
+            return None
+
+        # I need `if request.user.is_authenticated` because request
+        # always have a user attribute, usually an instance of
+        # AnonymousUser
+        if not request.user.is_authenticated:
+            return None
+
+        if hasattr(request.user, "jcomprofile"):
+            logger.warning(f"User {request.user.id} has no extended profile!")
+            if request.user.jcomprofile.gdpr_checkbox:
+                return None
+
+        message_text = """Please acknowledge privacy note (see checkbox below)
+        or log-out to continue navigate the site.
+        If you do not acknowledge pn, WRITE ME!!!
+        """
+        messages.add_message(
+            request,
+            messages.WARNING,
+            message_text,
+        )
+        logger.debug(
+            f"Redirecting {request.user.id} to profile page to acknowledge privacy."
+        )
+        return redirect(reverse("core_edit_profile"), permanent=False)
---
+++ wjs/jcom_profile/tests/test_middleware.py
@@ -0,0 +1,78 @@
+"""Tests related to the if-no-gdpr-acknowledged-redirect-me middleware."""
+import pytest
+from core.models import Account
+from django.test import Client
+
+
+@pytest.mark.django_db
+def test_anonymous_can_navigate(journal):
+    """Test that anonymous users can navigate.
+
+    Here "anonymous" means not logged in.
+    """
+    client = Client()
+    response = client.get("/contact/")
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_normal_user_can_navigate(journal):
+    """Test that a normal user can navigate normally.
+
+    A normal user has acknowledged our privay policy (i.e. checkbox is
+    selected).
+
+    """
+    # https://stackoverflow.com/a/39742798/1581629
+    client = Client()
+    user = Account.objects.get_or_create(username="testuser")[0]
+    user.is_active = True
+    user.jcomprofile.gdpr_checkbox = True
+    user.jcomprofile.save()
+    user.save()
+    client.force_login(user)
+
+    response = client.get("/contact/")
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_shy_user_cannot_navigate(journal):
+    """Test that a user that didn't acknowledge privacy cannot navigate."""
+    # check this also:
+    # https://flowfx.de/blog/test-django-with-selenium-pytest-and-user-authentication/
+    client = Client()
+    user = Account.objects.get_or_create(username="testuser")[0]
+    user.is_active = True
+    user.jcomprofile.gdpr_checkbox = False
+    user.jcomprofile.save()
+    user.save()
+    client.force_login(user)
+
+    response = client.get("/contact/")
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_shy_user_cannot_navigate_bis(journal, client):
+    """Test that a user that didn't acknowledge privacy cannot navigate.
+
+    Same as test_shy_user_cannot_navigate, but with a different way of
+    creating the user and of logging-in.
+
+    """
+    # https://pytest-django.readthedocs.io/en/latest/helpers.html#client-django-test-client
+    username = "user1"
+    password = "bar"
+    email = "e@mail.it"
+    user = Account.objects.create_user(
+        username=username, password=password, email=email
+    )
+    user.is_active = True
+    user.jcomprofile.gdpr_checkbox = False
+    user.jcomprofile.save()
+    user.save()
+    client.login(username=email, password=password)
+
+    response = client.get("/contact/")
+    assert response.status_code == 302
