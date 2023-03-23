23a11a15ad536ca3a22b48c73602b552f58aa6bc
Iacopo Spalletti <i.spalletti@nephila.digital>
2023-03-23 17:07:09 +0000

    Fix filtering articles in home page plugin

--- wjs/jcom_profile/tests/test_plugins.py
+++ wjs/jcom_profile/tests/test_plugins.py
@@ -1,17 +1,25 @@
 from datetime import timedelta
 
+import factory
 import pytest
 from django.contrib.contenttypes.models import ContentType
 from django.utils.timezone import now
 
-from wjs.plugins.wjs_latest_news.plugin_settings import get_plugin_context, WJSLatestArticles
-
-import factory
+from wjs.plugins.wjs_latest_articles.plugin_settings import WJSLatestArticles
+from wjs.plugins.wjs_latest_articles.plugin_settings import (
+    get_plugin_context as get_articles_plugin_context,
+)
+from wjs.plugins.wjs_latest_news.plugin_settings import (
+    WJSLatestArticles as WJSLatestnews,
+)
+from wjs.plugins.wjs_latest_news.plugin_settings import (
+    get_plugin_context as get_news_plugin_context,
+)
 
 
 @pytest.mark.django_db
 def test_news_context(fake_request, news_item_factory):
-    home_page_element = WJSLatestArticles.create_home_page_elements()
+    home_page_element = WJSLatestnews.create_home_page_elements()
 
     _now = now()
     news_count = 10
@@ -37,6 +45,28 @@ def test_news_context(fake_request, news_item_factory):
     news_item_factory.create_batch(
         news_count,
     )
-    context = get_plugin_context(fake_request, [home_page_element])
+    context = get_news_plugin_context(fake_request, [home_page_element])
     assert len(context["wjs_latest_news_list"]) == 7
     assert [n.sequence for n in context["wjs_latest_news_list"]] == [2, 3, 5, 6, 8, 9, 10]
+
+
+@pytest.mark.django_db
+def test_article_context(fake_request, article_factory):
+    home_page_element = WJSLatestArticles.create_home_page_elements()
+
+    _now = now()
+    items_count = 10
+    journal = fake_request.journal
+    # required to ensure batch object sequence always starts at 0
+    article_factory.reset_sequence()
+    # creating a set of articles:
+    # date_published date is set in the future for a subset of news (when index is divisible by 3)
+    article_factory.create_batch(
+        items_count,
+        journal=journal,
+        date_published=factory.Sequence(
+            lambda n: _now + timedelta(days=n) if n % 3 == 0 else _now - timedelta(days=n),
+        ),
+    )
+    context = get_articles_plugin_context(fake_request, [home_page_element])
+    assert len(context["wjs_latest_articles_list"]) == 7
