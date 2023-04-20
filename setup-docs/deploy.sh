f95fbfc28fae796290dce7a362d92a0f8e1ad6bd
Matteo Gamboz <gamboz@medialab.sissa.it>
2023-04-20 16:00:24 +0000

    review janeway/wjs deploy ci/cd

--- setup-docs/deploy.sh
+++ setup-docs/deploy.sh
@@ -34,21 +34,34 @@ PIP="${VENV_BIN}/pip"
 PYTHON="${VENV_BIN}/python"
 MANAGE_DIR="${JANEWAY_ROOT}/src"
 
-$PIP install --index-url="$PIP_INDEX_URL" -U "wjs.jcom-profile"
+case "$SSH_ORIGINAL_COMMAND" in
+    "deploy-janeway")
+        cd "$JANEWAY_ROOT"
+        git pull --ff-only https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.sissamedialab.it/wjs/janeway.git jcom
+        cd "$MANAGE_DIR"
+        "$PYTHON" manage.py migrate
+        "$PYTHON" manage.py collectstatic --noinput
+        ;;
 
-cd "$MANAGE_DIR"
+    *)
+        $PIP install --index-url="$PIP_INDEX_URL" -U "wjs.jcom-profile"
+        $PIP install --index-url="$PIP_INDEX_URL" -U "jcomassistant"
 
-"$PYTHON" manage.py link_plugins
-"$PYTHON" manage.py install_themes
+        cd "$MANAGE_DIR"
 
-"$PYTHON" manage.py migrate
+        "$PYTHON" manage.py link_plugins
+        "$PYTHON" manage.py install_themes
 
-"$PYTHON" manage.py build_assets
-"$PYTHON" manage.py collectstatic --noinput
+        "$PYTHON" manage.py migrate jcom_profile
 
-"$PYTHON" manage.py add_coauthors_submission_email_settings
-"$PYTHON" manage.py add_generic_analytics_code_setting
-"$PYTHON" manage.py add_publication_alert_settings
-"$PYTHON" manage.py add_user_as_main_author_setting
+        "$PYTHON" manage.py build_assets
+        "$PYTHON" manage.py collectstatic --noinput
+
+        "$PYTHON" manage.py add_coauthors_submission_email_settings
+        "$PYTHON" manage.py add_generic_analytics_code_setting
+        "$PYTHON" manage.py add_publication_alert_settings
+        "$PYTHON" manage.py add_user_as_main_author_setting
+        ;;
+esac
 
 touch --no-dereference "$UWSGI_VASSAL"
