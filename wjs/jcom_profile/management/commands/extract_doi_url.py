a9962ae11e9f182725ca4f63869c371e318698ac
Matteo Gamboz <gamboz@medialab.sissa.it>
2023-07-20 08:17:25 +0200

    Release 0.4.5
    - adapt DOI-URL extractor to crossref instructions

--- wjs/jcom_profile/management/commands/extract_doi_url.py
+++ wjs/jcom_profile/management/commands/extract_doi_url.py
@@ -1,4 +1,8 @@
-"""Data Extraction"""
+"""Prepare lists for bulk-update of resolution URLs.
+
+Crossref doc here:
+https://www.crossref.org/documentation/register-maintain-records/maintaining-your-metadata/updating-your-metadata/#00171
+"""
 import csv
 import os
 
@@ -6,6 +10,7 @@ from django.core.management.base import BaseCommand
 from journal.models import Journal
 from submission.models import STAGE_PUBLISHED, Article
 from utils.logger import get_logger
+from utils.setting_handler import get_setting
 
 logger = get_logger(__name__)
 
@@ -32,13 +37,20 @@ class Command(BaseCommand):
             default="/tmp",
             help="Where to store output csv files. Defaults to %(default)s",
         )
+        parser.add_argument(
+            "email",
+            help="Your email address (will be added to file header).",
+        )
 
     def extract_doi_url(self, dir_path, journal):
-        """extract doi url"""
-        file_name = journal.code + "_" + "doi_url.csv"
+        """Prepare lists for bulk-update of resolution URLs."""
+        prefix = get_setting("Identifiers", "crossref_prefix", journal).value
+        # Using .txt in the filename as exemplified in crossref doc:
+        # https://www.crossref.org/documentation/register-maintain-records/maintaining-your-metadata/updating-your-metadata/#00172
+        file_name = journal.code + "_" + "doi_url.txt"
         with open(dir_path + file_name, "w", newline="") as doi_url_csvfile:
-            writer = csv.writer(doi_url_csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL)
-            writer.writerow(["doi", "url"])
+            writer = csv.writer(doi_url_csvfile, delimiter="\t", quoting=csv.QUOTE_NONE)
+            writer.writerow([f"H: email={self.options['email']};fromPrefix={prefix}"])
             query_set = Article.objects.filter(journal=journal, stage=STAGE_PUBLISHED).order_by("-date_published")
             for article in query_set:
                 if doi := article.get_identifier("doi"):
--- wjs/jcom_profile/tests/test_extract_doi_url.py
+++ wjs/jcom_profile/tests/test_extract_doi_url.py
@@ -5,6 +5,7 @@ import pytest
 from django.core import management
 from django.utils import timezone
 from identifiers.models import Identifier
+from utils.setting_handler import save_setting
 
 
 @pytest.mark.django_db
@@ -18,8 +19,11 @@ def test_extract_doi_url(
     the data extraction. Output files in tmp_path
     """
     # The management command looks for journals with JCOM and JCOMAL codes
+    prefix = "PREFIX"
     jcom = journal_factory("JCOM")
+    save_setting("Identifiers", "crossref_prefix", jcom, prefix)
     jcomal = journal_factory("JCOMAL")
+    save_setting("Identifiers", "crossref_prefix", jcomal, prefix)
 
     # Setup first journal with one published article
     correspondence_author = account_factory()
@@ -66,19 +70,20 @@ def test_extract_doi_url(
 
     # call with optional argument store dir
     store_dir_arg = f"--store-dir={tmp_path}"
-    management.call_command("extract_doi_url", store_dir_arg)
+    my_email = "my@email.com"
+    management.call_command("extract_doi_url", store_dir_arg, my_email)
 
     # Check file content for first journal
-    with open(tmp_path / f"{jcom.code}_doi_url.csv") as csv_file:
-        csv_reader = csv.reader(csv_file)
+    with open(tmp_path / f"{jcom.code}_doi_url.txt") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter="\t")
         # expect the csv file to have two lines
         rows = list(csv_reader)
         assert len(rows) == 2
 
         # first line is for colum headers
         header_row = rows[0]
-        assert header_row[0] == "doi"
-        assert header_row[1] == "url"
+        assert len(header_row) == 1
+        assert header_row[0] == f"H: email={my_email};fromPrefix={prefix}"
 
         # second line contains our article's info
         data_row = rows[1]
@@ -87,21 +92,21 @@ def test_extract_doi_url(
         assert "article/pubid/JCOM" in data_row[1]
 
     # Check file content for second journal
-    with open(tmp_path / f"{jcomal.code}_doi_url.csv") as csv_file_two:
-        csv_reader = csv.reader(csv_file_two)
+    with open(tmp_path / f"{jcomal.code}_doi_url.txt") as csv_file_two:
+        csv_reader = csv.reader(csv_file_two, delimiter="\t")
         # expect the csv file to have two lines
         rows = list(csv_reader)
         assert len(rows) == 2
 
         # first line is for colum headers
         header_row = rows[0]
-        assert header_row[0] == "doi"
-        assert header_row[1] == "url"
+        assert len(header_row) == 1
+        assert header_row[0] == f"H: email={my_email};fromPrefix={prefix}"
 
         # second line contains our article's info
         data_row = rows[1]
         assert data_row[0] == article_two.get_identifier("doi")
         assert data_row[1] == article_two.url
-        assert "article/pubid/JCOM" not in data_row[1]
+        assert "article/pubid/JCOMAL" not in data_row[1]
 
     assert True
