2c85cc031ea22c6c265e67afad6ab7ab495ddce6
Matteo Gamboz <gamboz@medialab.sissa.it>
2023-12-07 14:41:37 +0000

    Add a per-month count of published papers and registered DOI

--- wjs/plugins/wjs_stats/plugin_settings.py
+++ wjs/plugins/wjs_stats/plugin_settings.py
@@ -4,6 +4,9 @@ from pathlib import Path
 from typing import Any, Dict
 
 from utils import plugins
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 PLUGIN_NAME = "WJS Stats"
 DISPLAY_NAME = "WJS Stats"
@@ -13,6 +16,9 @@ VERSION = "0.1"
 SHORT_NAME = str(Path(__file__).parent.name)
 MANAGER_URL = "wjs_stats_manager"
 JANEWAY_VERSION = "1.5.0"
+
+# The name of the group "Accounting"
+GROUP_ACCOUNTING = "Accounting"
 
 
 class WJSStats(plugins.Plugin):
@@ -29,8 +35,21 @@ class WJSStats(plugins.Plugin):
 def install():
     """Register the plugin instance."""
     WJSStats.install()
+    ensure_accounting_group_exists()
 
 
 def hook_registry() -> Dict[str, Any]:
     """Register hooks for current plugin."""
     return {}
+
+
+def ensure_accounting_group_exists():
+    """Add a group called accounting.
+
+    Users belonging to this group can access the DOI-count pages even if they are not staff.
+    """
+    from django.contrib.auth.models import Group
+
+    group, created = Group.objects.get_or_create(name=GROUP_ACCOUNTING)
+    if created:
+        logger.info(f"Created group {group.name}.")
--- wjs/plugins/wjs_stats/views.py
+++ wjs/plugins/wjs_stats/views.py
@@ -1,15 +1,21 @@
 """Views."""
+import calendar
 from io import BytesIO
 
 import mariadb
 import requests
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Min
 from django.http import FileResponse
 from django.utils import timezone
-from django.views.generic import TemplateView, View
+from django.views.generic import ListView, TemplateView, View
+from identifiers.models import CrossrefStatus
 from requests.auth import HTTPBasicAuth
+from submission.models import Article
 from utils.logger import get_logger
+
+from .plugin_settings import GROUP_ACCOUNTING
 
 # TODO: add specific permission to plugin and use PermissionRequiredMixin?
 logger = get_logger(__name__)
@@ -107,3 +113,80 @@ class RecipientsCount(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
     def test_func(self):
         """Verify that only staff can see recipients count."""
         return self.request.user.is_staff
+
+
+class DOIsCount(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    """Show number of registered DOIs by journal by month."""
+
+    template_name = "wjs_stats/dois_count.html"
+    model = CrossrefStatus
+
+    def test_func(self):
+        """Only staff or memebers of the "Accounting" group."""
+        return self.request.user.is_staff or self.request.user.groups.filter(name=GROUP_ACCOUNTING).exists()
+
+    def get_queryset(self):
+        """Group DOIs by their first registration date, as extracted from Crossrefdeposit.document."""
+        return CrossrefStatus.objects.filter(
+            identifier__article__journal__code=self.request.journal.code,
+            identifier__id_type="doi",
+            deposits__success=True,
+        ).annotate(min_d=Min("deposits__date_time"))
+
+    def get_context_data(self, **kwargs):
+        """Add a count of DOIs and published papers per year-month."""
+        context = super().get_context_data(**kwargs)
+        qs = self.get_queryset()
+        result = []
+        now = timezone.now()
+        oldest_publication_date = (
+            Article.objects.filter(
+                date_published__isnull=False,
+                journal=self.request.journal,
+            )
+            .order_by("date_published")
+            .values_list("date_published", flat=True)[0]
+        )
+
+        # I was not able to find the appropriate recipe of filter/annotate/aggregate to extract these infor directly
+        # from the DB (see experiments in last comments of specs#428).  Shame on me!
+        for year in range(now.year, oldest_publication_date.year - 1, -1):
+            for month in range(12, 0, -1):
+                if year == now.year and month > now.month:
+                    continue
+                if year == oldest_publication_date.year and month < oldest_publication_date.month:
+                    continue
+                # Let's pass along also a "note" indicating when the journal moved to Janeway
+                note = ""
+                if year == 2023:
+                    if month == 3:
+                        if self.request.journal.code == "JCOM":
+                            note = "JCOM to Janeway"
+                    elif month == 5:
+                        if self.request.journal.code == "JCOMAL":
+                            note = "JCOMAL to Janeway"
+
+                published_papers = Article.objects.filter(
+                    journal=self.request.journal,
+                    date_published__year=year,
+                    date_published__month=month,
+                ).count()
+
+                registered_dois = qs.filter(
+                    min_d__month=month,
+                    min_d__year=year,
+                ).count()
+                date = timezone.datetime(year, month, 1)
+                result.append(
+                    {
+                        "date": date,
+                        "month_end": date.strftime(f"%Y-%m-{calendar.monthrange(date.year, date.month)[1]}"),
+                        "year_month": date.strftime("%Y-%m"),
+                        "papers": published_papers,
+                        "dois": registered_dois,
+                        "note": note,
+                    },
+                )
+
+        context["counts"] = result
+        return context
