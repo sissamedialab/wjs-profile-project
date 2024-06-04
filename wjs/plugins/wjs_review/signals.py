409d7be87f420f14c62ff7575d16a07d91ac5989
Lorenzo Gramaccioni <l.gramaccioni@nephila.digital>
2024-06-04 08:40:04 +0000

    Last state transition date

--- wjs/plugins/wjs_review/signals.py
+++ wjs/plugins/wjs_review/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
+from django_fsm.signals import post_transition
 from submission.models import Article
 
 from .models import ArticleWorkflow
@@ -11,3 +13,9 @@ def create_workflow_handler(sender, instance, created, **kwargs):
     if not created:
         return
     ArticleWorkflow.objects.create(article=instance)
+
+
+@receiver(post_transition, sender=ArticleWorkflow)
+def log_state_change(instance, **kwargs):
+    instance.latest_state_change = timezone.now()
+    instance.save()
