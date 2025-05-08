f4e02782a82fbafa4d12d46db423aa0c0378dcb3
Matteo Gamboz <gamboz@medialab.sissa.it>
2025-05-08 13:25:43 +0200

    Release 1.0.11
    Call "apply-wjs-settings" from "run-customizations" with "--noinput",
    otherwise this fails in `DEBUG=False` instances

--- wjs/jcom_profile/management/commands/run_customizations.py
+++ wjs/jcom_profile/management/commands/run_customizations.py
@@ -16,4 +16,4 @@ class Command(BaseCommand):
         management.call_command("install_themes")
         management.call_command("link_plugins")
         management.call_command("create_role", "Director")
-        management.call_command("apply_wjs_settings")
+        management.call_command("apply_wjs_settings", "--noinput")
