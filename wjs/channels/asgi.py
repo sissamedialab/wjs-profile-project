b94911d3f2343078943cc2975d3eb79caf52821d
Matteo Gamboz <gamboz@medialab.sissa.it>
2025-11-12 10:21:10 +0100

    otel instrumentation - first attempts (logs are missing)

--- wjs/channels/asgi.py
+++ wjs/channels/asgi.py
@@ -13,7 +13,31 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from opentelemetry import metrics, trace
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.auto_instrumentation import initialize
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from utils import load_janeway_settings
+
+# Service name is required for most backends
+resource = Resource.create(attributes={SERVICE_NAME: "your-service-name"})
+
+tracerProvider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="your-endpoint-here"))
+tracerProvider.add_span_processor(processor)
+trace.set_tracer_provider(tracerProvider)
+
+reader = PeriodicExportingMetricReader(OTLPMetricExporter(endpoint="localhost:5555"))
+meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
+metrics.set_meter_provider(meterProvider)
+
+initialize()
+
 
 # early init for django app
 os.environ.setdefault("JANEWAY_SETTINGS_MODULE", "core.settings")
